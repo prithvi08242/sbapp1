628b80445fb64080d74b865c60dde85355e21636
Prithvi <prithvi@Prithvis-MacBook-Pro.local>
2025-09-03 22:51:24 +0530

    Your descriptive commit message

---
+++ src/main/java/com/prithvi/Sbapp1DockerApplication.java
@@ -0,0 +1,13 @@
+package com.prithvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbapp1DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbapp1DockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prithvi/Sbapp1DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prithvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbapp1DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
